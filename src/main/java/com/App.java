bd29b92a5b81a93e5a27e30cd4fdcc396d51cb62
lucyu2217625 <lucyu2217625@gmail.com>
2021-09-05 21:53:05 +0800

    新增compiler

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,4 +1,6 @@
-package com.stupid;
+package com;
+import java.io.IOException;
+
 import com.compiler.*;
 //import com.Window.*;
 
@@ -8,15 +10,22 @@ import com.compiler.*;
 public final class App {
     private App() {
     }
-
+    
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        new compiler();
-        /*
+    
+    public static void main(String[] args) throws IOException {
+
+        compiler abc=new compiler();
+        
+        abc.file_path="C:/Users/User/Desktop/1/";
+        abc.filename="test.java";
+        abc.p();
         System.out.println("Hello World!");
+        /*
         new Window(); // new 出Windows(已經繼承了Frame)
         */
     
--- src/main/java/com/compiler/compiler.java
+++ src/main/java/com/compiler/compiler.java
@@ -1,23 +1,43 @@
 package com.compiler;
-
+import javax.tools.*;
+import java.io.*;
 
 public class compiler 
 {
-    public compiler()
+    public String file_path; // 檔案位置
+    public String filename; // 檔名
+
+    public compiler()throws IOException
     {
-        System.out.println("Hello");    
-        String directoryName = System.getProperty("user.dir");
-		System.out.println("Current Working Directory is = " +directoryName);
-        /*
-        */
-        io link = new io();
-        link.filename="Test1.txt";
-        link.File_maker();
-        link.Reader_();
-        link.content="Hello world";
-        link.Writer_();
-        link.Reader_();
-        System.out.println(link.get_path);
+        
         
     }
+    public void p()throws IOException
+    {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+        int results = compiler.run(null, null, null, file_path + filename);
+
+        System.out.println((results == 0)?"編譯成功":"編譯失敗");
+
+        // 在程式中執行test
+
+        Runtime run = Runtime.getRuntime();
+
+        Process p = run.exec("java " + file_path + filename);
+
+        BufferedInputStream in = new BufferedInputStream(p.getInputStream());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String s;
+
+        while ((s = br.readLine()) != null)
+
+        System.out.println(s);
+    }
+
+
 }
+
+
