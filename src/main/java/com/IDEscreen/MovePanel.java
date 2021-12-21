5be933bfe51cd211ae2400edb793562f7e473a0f
ying90807 <90807ian@gmail.com>
2021-12-21 09:49:25 +0800

    test

--- src/main/java/com/IDEscreen/MovePanel.java
+++ src/main/java/com/IDEscreen/MovePanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.border.*;
 import java.awt.geom.*;
-import java.awt.*;
+import java.awt.*;   
 import java.awt.event.*;
 
 public class MovePanel extends JPanel {
